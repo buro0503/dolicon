64b84e87053a9fa1622e313a12b58e6098dd957b
IshifuroShogo <i14261426@yahoo.co.jp>
2022-04-16 15:35:12 +0900

    パスワード再設定1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,13 +36,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'https://dolicon.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host,
-    :authentication => :plain,
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV["GOOGLE_MAIL_ADDRESS"],
+    :password => ENV["GOOGLE_MAILER_PASSWORD"],
+    :authentication => 'login'
   }
   Rails.application.routes.default_url_options[:host] = host
   # Specifies the header that your server uses for sending files.
