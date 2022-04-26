4839650b837c7bfcfcf4e71a7723977fdfc5acc9
IshifuroShogo <i14261426@yahoo.co.jp>
2022-04-27 00:14:57 +0900

    metakesu

--- db/migrate/20220426022948_add_nicname_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNicnameToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :nicname, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_26_022948) do
+ActiveRecord::Schema.define(version: 2022_04_21_062812) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -79,7 +79,6 @@ ActiveRecord::Schema.define(version: 2022_04_26_022948) do
     t.text "name"
     t.integer "user_id"
     t.text "ryakusyou"
-    t.text "nicname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
