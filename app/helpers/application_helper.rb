73f0f3ff19aca9c20e169e388c338edef01d94dc
IshifuroShogo <i14261426@yahoo.co.jp>
2022-04-27 12:35:38 +0900

    調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,14 +17,17 @@ module ApplicationHelper
           title: :full_title,          # :full_title とすると、サイトに表示される <title> と全く同じものを表示できる
           description: :full_description,   # 上に同じ
           url: request.url,
-          image: 'https://example.com/hoge.png'
+          image: '/twitter_image.png'
         }
       end
       
       def default_twitter_card
         {
-          card: 'summary_large_image', # または summary
-          site: '@buro_gm'            # twitter ID
+          card: 'summary', # または summary
+          site: '@buro_gm',            # twitter ID
+          title: 'マンドリン演奏会情報サイト',
+          description: '全国のマンドリンの演奏会情報を投稿・閲覧できるサイトです',
+          image: '/twitter_image.png'
         }
       end
 end
