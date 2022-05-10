8592557b941d5f1e400786a32145d091805bfe34
IshifuroShogo <i14261426@yahoo.co.jp>
2022-05-10 18:38:55 +0900

    ○○.index.erbを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,7 +28,7 @@ module ApplicationHelper
           site: '@buro_gm',            # twitter ID
           title: 'マンドリン演奏会情報サイト',
           description: '全国のマンドリンの演奏会情報を投稿・閲覧できるサイトです',
-          image: image_url("twitter/twitter_image.jpg")
+          image: image_url("twitter/twitter_image.png")
         }
       end
 end
