b0a37c153247a9b236bf30be12ce0ba3734f45ef
IshifuroShogo <i14261426@yahoo.co.jp>
2022-05-10 17:49:12 +0900

    ○○.index.erbを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,7 @@ module ApplicationHelper
           separator: '|',
           og: defalut_og,
           twitter: default_twitter_card
+          image: '/twitter_image.png'
         }
       end
       
