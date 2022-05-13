618f6b094ff257872e83c71954b1eea2a96ff8bc
IshifuroShogo <i14261426@yahoo.co.jp>
2022-05-13 16:04:14 +0900

    ○○.index.erbを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,8 +16,8 @@ module ApplicationHelper
       
       def defalut_og
         {
-          site_name: 'マンドリン演奏会情報サイト',
-          title: full_title,          # :full_title とすると、サイトに表示される <title> と全く同じものを表示できる
+          site_name: 'マンドリン演奏会情報サイト.',
+          title: :full_title,          # :full_title とすると、サイトに表示される <title> と全く同じものを表示できる
           description: :full_description,   # 上に同じ
           url: request.url,
           image: image_url('twitter/line_image.png')
