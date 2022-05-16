6c666ec4101ec6bc07cf179bf62d7ae02ccecf69
IshifuroShogo <i14261426@yahoo.co.jp>
2022-05-16 12:12:55 +0900

    ○○.index.erbを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,4 +32,15 @@ module ApplicationHelper
           image: image_url('twitter/twitter_image.png')
         }
       end
+
+        require 'uri'  
+          def convert_url_into_a_tag(text)
+            URI.extract(text, ['http', 'https']).uniq.each do |url|
+              sub_text = ""
+              sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+        
+              text.gsub!(url, sub_text)
+            end
+           return text
+          end
 end
