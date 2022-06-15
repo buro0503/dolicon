86032e2cbeca7e0d57c1c8305657be83811a9a92
IshifuroShogo <i14261426@yahoo.co.jp>
2022-06-15 12:47:27 +0900

    a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -157,7 +157,7 @@ end
     params.require(:post).permit(:name1, :name2, :month, :day, :detail,
      :image, :image2, :music, :site, :post, :prefecture, :prefecture_id,
       :latitude, :longitude, :date, :start_time, :image1, :image2,
-      :title, :details, :hikukainame, :hikukairyakusyou, :postsyurui, :kyoudou1, :kyoudou2, :kyoudou3, :kyoudou4, :kyoudou5)
+      :title, :details, :hikukainame, :hikukairyakusyou, :postsyurui, :kyoudou1, :kyoudou2, :kyoudou3, :kyoudou4, :kyoudou5, :address)
  end
 
  def post_search_params
